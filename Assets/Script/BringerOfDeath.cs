f272439937af7cf16cac82d46e89048fc251d481
manxuan8231 <lydangxuanman05@gmail.com>
2024-07-12 23:41:07 +0700

    newBoss

--- Assets/Script/BringerOfDeath.cs
+++ Assets/Script/BringerOfDeath.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
@@ -7,16 +8,19 @@ using UnityEngine.UI;
 public class BringerOfDeath : MonoBehaviour
 {
 
-
+    public float detectionRangeAttack = 2.5f;  // Phạm vi phát hiện người chơi
     public float detectionRange = 7f;  // Phạm vi phát hiện người chơi
     public Transform Player;//follow player
-    private bool player;
+   
 
     public Slider healthSlider;//slider hp boss
     private int health;
 
     public GameObject attackSkill;//skill
     public Transform attack;//vị trí tấn công
+
+   
+    
 
     private float TimeAttackRate = 2f;
     private float timeAttack;
@@ -33,19 +37,21 @@ public class BringerOfDeath : MonoBehaviour
         health = 1000;
         healthSlider.maxValue = health;
         timeAttack = TimeAttackRate;
-
+        
     }
 
     void Update()
     {
         followPlayer();
         TimeAttack();//độ trể khi thấy player sau 3f tấn công
-
+       
 
     }
+  
     void TimeAttack()
     {
-        if (player)
+        float distanceToPlayer = Vector3.Distance(transform.position, Player.position);
+        if (distanceToPlayer < detectionRangeAttack)
         {
             timeAttack -= Time.deltaTime;
             if (timeAttack <= 0)
@@ -82,44 +88,39 @@ public class BringerOfDeath : MonoBehaviour
                 transform.localScale = kichThuoc;
                 animator.SetTrigger("isTele");
             }
-        }
-
-
+        }       
     } 
     private void OnTriggerEnter2D(Collider2D other)
     {
+       
         if (other.gameObject.CompareTag("Shuriken"))//nếu chạm shuriken thì mất máu
-        {
-            
+        {            
             health -= 5;
             healthSlider.value = health;
-            
+           
             animator.SetTrigger("isHurt");
             Destroy(other.gameObject);//shuriken biến mất
         }
-        if (other.gameObject.CompareTag("Player"))
-        {
-            player = true;           
-        }
+        
         if (other.gameObject.CompareTag("Attack1"))
         {
             health -= 10;
             healthSlider.value = health;
-
+           
             animator.SetTrigger("isHurt");
         }
         if (other.gameObject.CompareTag("Attack2"))
         {
             health -= 20;
             healthSlider.value = health;
-
+           
             animator.SetTrigger("isHurt");
         }
         if (other.gameObject.CompareTag("Attack3"))
         {
             health -= 30;
             healthSlider.value = health;
-
+            
             animator.SetTrigger("isHurt");
         }
         if (other.gameObject.CompareTag("SpecialAttack"))
@@ -128,20 +129,18 @@ public class BringerOfDeath : MonoBehaviour
             health -= 40;
             healthSlider.value = health;
             animator.SetTrigger("isHurt");
-
+           
         }
 
         if (health <= 0)
         {
-            Destroy(gameObject);
+            Destroy(gameObject,1f);
+            animator.SetTrigger("isDeath");
         }
     }
     private void OnTriggerExit2D(Collider2D other)
     {
-        if (other.gameObject.CompareTag("Player"))
-        {
-            player = false;
-        }
+       
        
     }
 }
