c136b6739152e1b020a85c07a63ab24095032bbb
PS41593 <huudat5234@gmail.com>
2024-07-22 18:42:09 +0700

    trap

--- Assets/Script/Trapground.cs
+++ Assets/Script/Trapground.cs
@@ -1,18 +1,37 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class Trapground : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform bay;
+    public GameObject pipePrefab; 
+    public List<GameObject> pipes;
+    public float timeToCreatePipes;
+    int numberOfPipes = 0;
     void Start()
     {
-        
+        timeToCreatePipes = 2f;
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        timeToCreatePipes -= Time.deltaTime;
+        if (timeToCreatePipes <= 0)
+        {
+            CreateRandomPipes();
+            timeToCreatePipes = 2f;
+        }
+    }
+    private void CreateRandomPipes()
+    {
+        var randomY = Random.Range(-3f, 3f);
+        var pipe = Instantiate(pipePrefab,bay.transform.position, Quaternion.identity);
+        Destroy(pipe, 10);
+        MovePipes(pipe);
+    }
+    private void MovePipes(GameObject pipe)
+    {
+        pipe.GetComponent<Rigidbody2D>().velocity = Vector2.left * 2;
     }
 }
