5c9c9f3c66fda3e81fd6d892855093cdb6682c76
PS41593 <huudat5234@gmail.com>
2024-07-23 14:36:48 +0700

    Trap ban

---
+++ Assets/Script/Trapbullet.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.Mathematics;
+using UnityEditor;
+using UnityEngine;
+
+public class Trapbullet : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform bulletPos;
+
+    public float timer;
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime; 
+        if(timer > 2)
+        {
+            timer =0 ;
+            shoot();
+        }
+    }
+    void shoot()
+    {
+        Instantiate(bullet,bulletPos.position,Quaternion.identity);
+    }
+
+}
---
+++ Assets/Script/bullet.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    private GameObject player; 
+    private Rigidbody2D rb;
+    public float force;
+    private void Start()
+    {
+         rb = GetComponent<Rigidbody2D>();
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        Vector3 direction = player.transform.position - transform.position; 
+        rb.velocity = new Vector2 (direction.x, direction.y).normalized * force;
+
+        float rot = Mathf.Atan2(-direction.y, -direction.x)* Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, rot);
+    }
+}
