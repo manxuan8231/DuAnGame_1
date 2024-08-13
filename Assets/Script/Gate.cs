024f8140127786647505a125a1c89df567ac4198
name1806 <hongnhut11112005@gmail.com>
2024-08-14 00:58:13 +0700

    .

---
+++ Assets/Script/Gate.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Gate : MonoBehaviour
+{
+    [SerializeField] private GameObject _loandingCanvas;
+    [SerializeField] private Slider _slider;
+    [SerializeField] TextMeshProUGUI _progressText;
+    private float _progress = 0;
+    private void Start()
+    {
+        _loandingCanvas.SetActive(false);
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            // hiển thị màng hình loanding
+            _loandingCanvas.SetActive(true);
+            _slider.value = _progress;
+            _progressText.text = _progress + "%";
+            StartCoroutine(LoandScens());
+        }
+
+        IEnumerator LoandScens()
+        {
+            while (_progress < 100)
+            {
+                _progress += 1;
+                _slider.value = _progress;
+                _progressText.text = _progress + "%";
+                yield return new WaitForSeconds(0.1f);
+            }
+            // chuyển màng chơi
+            SceneManager.LoadScene(1);
+
+        }
+    }
+
+}
